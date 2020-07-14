2a676d6d62ee0a8c9701f6cc145234933f250ceb
Rich Wasek <rideburtonrw@gmail.com>
2020-07-14 09:45:06 -0600

    First commit

---
+++ DB/jobtrackerdb.sql
@@ -0,0 +1,86 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema jobtrackerdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `jobtrackerdb` ;
+
+-- -----------------------------------------------------
+-- Schema jobtrackerdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `jobtrackerdb` DEFAULT CHARACTER SET utf8 ;
+USE `jobtrackerdb` ;
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(50) NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `enabled` TINYINT NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `job`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `job` ;
+
+CREATE TABLE IF NOT EXISTS `job` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `company_name` VARCHAR(200) NOT NULL,
+  `date_applied` DATE NOT NULL,
+  `job_title` VARCHAR(100) NOT NULL,
+  `location` VARCHAR(150) NOT NULL,
+  `contact` VARCHAR(45) NULL,
+  `cover_letter` TINYINT NULL,
+  `status` ENUM('PENDING', 'DENIED', 'OFFER') NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_job_user_idx` (`user_id` ASC),
+  CONSTRAINT `fk_job_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS jobuser;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'jobuser' IDENTIFIED BY 'jobuser';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'jobuser';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `user`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `jobtrackerdb`;
+INSERT INTO `user` (`id`, `first_name`, `username`, `password`, `enabled`) VALUES (1, 'Rich', 'rwasek@localhost', 'rwasek', 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `job`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `jobtrackerdb`;
+INSERT INTO `job` (`id`, `company_name`, `date_applied`, `job_title`, `location`, `contact`, `cover_letter`, `status`, `user_id`) VALUES (1, 'Progrexion', '2020-07-11', 'Java Software Developer', 'Salt Lake City, UT', 'NULL', 0, 'PENDING', 1);
+
+COMMIT;
+
